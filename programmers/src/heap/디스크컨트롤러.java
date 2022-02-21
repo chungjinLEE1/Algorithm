801147044403ef4bc54825a4d9e428e2ad6312a7
chungjinLEE1 <cjlee1@brinks.co.kr>
2022-02-21 13:26:16 +0900

    디스크 컨트롤러

---
+++ programmers/src/heap/디스크컨트롤러.java
@@ -0,0 +1,49 @@
+package heap;
+
+import java.util.Arrays;
+import java.util.PriorityQueue;
+
+public class 디스크컨트롤러 {
+
+    public static void main(String[] args) {
+        int[][] jobs = {{0,3}, {1, 9}, {2, 6}};
+        디스크컨트롤러 s = new 디스크컨트롤러();
+
+        System.out.println(s.solution(jobs));
+
+
+
+    }
+
+
+    public int solution(int[][] jobs) {
+        int answer = 0;
+        int count = 0; // 처리된 디스크
+        int now = 0; // 작업이 끝난 시간
+
+
+        Arrays.sort(jobs, (o1, o2) -> o1[0] - o2[0]);
+
+
+        PriorityQueue<int[]> queue = new PriorityQueue<>(((o1, o2) -> o1[1]- o2[1]));
+        int i =0;
+        while(count < jobs.length){
+            while(i <jobs.length && jobs[i][0] <= now){
+                queue.add(jobs[i++]);
+            }
+
+
+            if(queue.isEmpty()){
+                now = jobs[i][0];
+            }else {
+                int[] tmp = queue.poll();
+                answer += tmp[1] + now - tmp[0];
+                now += tmp[1];
+                count++;
+
+            }
+        }
+
+        return answer / jobs.length;
+    }
+}
