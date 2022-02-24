3ed24795d9067c78f21f9987c3ef2c9e34d43ee3
chungjinLEE1 <cjlee1@brinks.co.kr>
2022-02-24 14:39:40 +0900

    가장큰수

---
+++ programmers/src/sort/가장큰수2.java
@@ -0,0 +1,25 @@
+package sort;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class 가장큰수2 {
+
+    public String solution(int[] numbers){
+        String answer = "";
+
+        ArrayList<String> strNumbers = new ArrayList<>();
+        for(Integer num : numbers){
+            strNumbers.add(String.valueOf(num));
+        }
+
+        Collections.sort(strNumbers, (a, b) -> (b + a).compareTo(a + b));
+
+        if(strNumbers.get(0).startsWith("0")) return "0";
+
+        for(String s : strNumbers){
+            answer += s;
+        }
+        return answer;
+    }
+}
